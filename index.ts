c348b11330e42737b045afbe4578b1390d737cb3
Hubab777 <159882654+Hubab777@users.noreply.github.com>
2024-10-18 22:48:21 +0500

    Great!

---
+++ index.ts
@@ -0,0 +1,47 @@
+// A simple Calculator that can perform basic arithmetic operations.
+
+import inquirer from "inquirer";
+
+// Asking questions from users through inquirer
+
+async function runCalculator() {
+
+const answers = await inquirer.prompt([
+{ message: "Enter First Number", 
+   type: "number", 
+   name: "firstNumber" },
+
+{ message: "Enter Second Number", 
+   type: "number", 
+   name: "secondNumber" },
+{ 
+     message: "Select an Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+ },
+
+]);
+
+console.log(answers);
+
+//Conditional statements if-else
+
+if(answers.operator==="Addition"){
+   console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.operator==="Subtraction"){
+   console.log(answers.firstNumber - answers.secondNumber)
+}
+else if(answers.operator==="Multiplication"){
+   console.log(answers.firstNumber * answers.secondNumber)
+}
+else if(answers.operator==="Division"){
+   console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+   console.log('Invalid Input')
+}
+};
+
+runCalculator();
